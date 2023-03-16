252a58aafccfbdf3a6ca2a4e6d422587854e2a2b
nirban <nirban.das@gmail.com>
2023-03-16 05:40:03 +0530

    adding requirements.txt

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+        this func will return the list of requirements
+    '''
+
+    requirements = []
+
+    with open(file_path) as file_ob:
+        requirements = file_ob.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="nirban",
+    author_email="nirban.das@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
+
