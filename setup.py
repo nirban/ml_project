5111ec0767aee3c0324205f7ddcc12ff41cff4ee
nirban <nirban.das@gmail.com>
2023-03-16 05:54:01 +0530

    adding all

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = "-e ."
+
 def get_requirements(file_path:str)->List[str]:
     '''
         this func will return the list of requirements
@@ -11,6 +13,11 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_ob:
         requirements = file_ob.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
         
 
 setup(
